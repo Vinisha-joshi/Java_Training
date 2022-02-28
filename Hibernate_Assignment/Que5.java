5bfe661878113b7671bd7f0d12ca33473cefb730
Vinisha-joshi <87906540+Vinisha-joshi@users.noreply.github.com>
2022-02-28 15:20:08 +0530

    Add files via upload

---
+++ Hibernate_Assignment/Que5.java
@@ -0,0 +1,12 @@
+package Hibernate_Assignment;
+
+public class Que5 {
+    /*
+        Q5 What are the benefits of using Hibernate Criteria API over regular SQL statements
+        ->The advantages of the Criteria API is that it is intuitively designed to manipulate data without
+         using any hard-coded SQL statements.
+        ->Programmatic behavior offers compile-time syntax checking; as a result, most error messages are
+         delivered during compilation
+    
+     */
+}
---
+++ Hibernate_Assignment/Que6.java
@@ -0,0 +1,36 @@
+package Hibernate_Assignment;
+
+public class Que6 {
+    /*
+        Q6 Given an example of each of the following relationships
+          1:1, 1:M, M: M, M:1 (one to one, one to many, many to many, many to one)
+
+        Ans-->1) One-to-One Relationship: Such a relationship exists when each record of one table is related
+        to only one record of the other table.
+        For example, If there are two entities ‘Person’ (Id, Name, Age, Address)and
+        ‘Passport’(Passport_id, Passport_no). So, each person can have only one passport and
+        each passport belongs to only one person.
+
+        2)One-to-Many or Many-to-One Relationship : Such a relationship exists when each record of one table can be related to one or
+        more than one record of the other table. This relationship is the most common relationship found.
+        A one-to-many relationship can also be said as a many-to-one relationship depending upon the
+        way we view it.
+        For example, If there are two entity type ‘Customer’ and ‘Account’ then each ‘Customer’ can have
+        more than one ‘Account’ but each ‘Account’ is held by only one ‘Customer’. In this example, we
+        can say that each Customer is associated with many Account. So, it is a one-to-many relationship.
+        But, if we see it the other way i.e many Account is associated with one Customer then we can say
+        that it is a many-to-one relationship.
+
+        3)Many-to-Many Relationship: Such a relationship exists when each record of the first table can
+        be related to one or more than one record of the second table and a single record of the second
+        table can be related to one or more than one record of the first table. A many-to-many relationship
+        can be seen as a two one-to-many relationship which is linked by a 'linking table' or 'associate
+        table'. The linking table links two tables by having fields which are the primary key of the other
+        two tables. We can understand this with the following example.
+        For Example: If there are two entity type ‘Customer’ and ‘Product’ then each customer can buy more
+        than one product and a product can be bought by many different customers.
+
+
+
+     */
+}
