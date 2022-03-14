969bfb991af40afc523af9d4c91326e4b7ad79e1
Vinisha-joshi <87906540+Vinisha-joshi@users.noreply.github.com>
2022-03-14 12:33:47 +0530

    Delete Spring_Assignment/Que1 directory

--- Spring_Assignment/Que1/MySql.java
+++
@@ -1,5 +0,0 @@
-package Spring_Assignment.Que1;
-
-public interface MySql {
-    void operation();
-}
--- Spring_Assignment/Que1/operation1.java
+++
@@ -1,8 +0,0 @@
-package Spring_Assignment.Que1;
-
-public class operation1 implements MySql {
-    @Override
-    public void operation() {
-        System.out.println("operation1");
-    }
-}
