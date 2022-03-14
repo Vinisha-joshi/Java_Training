899e7443eeeee2c9bad5a078c02f34fd0e4443d0
Vinisha-joshi <87906540+Vinisha-joshi@users.noreply.github.com>
2022-03-14 12:36:11 +0530

    Add files via upload

---
+++ Spring_Assignment/Que1/operation1.java
@@ -0,0 +1,8 @@
+package Spring_Assignment.Que1;
+
+public class operation1 implements MySql {
+    @Override
+    public void operation() {
+        System.out.println("operation1");
+    }
+}
---
+++ Spring_Assignment/Que2/User.java
@@ -0,0 +1,4 @@
+package Spring_Assignment.Que2;
+
+public class User {
+}
